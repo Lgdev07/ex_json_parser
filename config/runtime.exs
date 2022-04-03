cecd4ad7f96e449671fb85db5e0eb1c85b1687d7
Luan Gomes <luan_saopaulo06@hotmail.com>
2022-04-03 20:05:21 -0300

    fix runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,7 +29,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :ex_json_parser, ExJsonParserWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
