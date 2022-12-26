16677cb287497696bf12c97f3bd62a16f689c45f
Lgdev07 <luan_saopaulo06@hotmail.com>
2022-12-26 21:03:31 +0100

    add elixir to json

--- lib/ex_json_parser_web/live/home_live.ex
+++ lib/ex_json_parser_web/live/home_live.ex
@@ -17,16 +17,8 @@ defmodule ExJsonParserWeb.HomeLive do
 
   @impl true
   def handle_event(_action, values, socket) do
-    json = Map.get(values, "json")
-
-    case Jason.decode(json) do
-      {:ok, elixir_map} ->
-        socket =
-          socket
-          |> assign(json: json)
-          |> assign(error: nil)
-          |> assign(elixir_map: elixir_map)
-
+    case format_json(values, socket) do
+      {:ok, socket} ->
         {:noreply, socket}
 
       {:error, _reason} ->
@@ -36,6 +28,38 @@ defmodule ExJsonParserWeb.HomeLive do
           |> assign(elixir_map: "")
 
         {:noreply, socket}
+    end
+  end
+
+  defp format_json(
+         %{"json" => new_json, "elixir_map" => new_elixir_map},
+         %{assigns: %{json: prev_json, elixir_map: prev_elixir_map}} = socket
+       ) do
+    cond do
+      new_json != prev_json ->
+        with {:ok, new_elixir_map} <- Jason.decode(new_json) do
+          {:ok,
+           socket
+           |> assign(json: new_json)
+           |> assign(error: nil)
+           |> assign(elixir_map: inspect(new_elixir_map))}
+        end
+
+      new_elixir_map != prev_elixir_map ->
+        with {:ok, {:%{}, _, _}} <- Code.string_to_quoted(new_elixir_map),
+             {new_elixir_map, []} <- Code.eval_string(new_elixir_map),
+             {:ok, new_json} <- Jason.encode(new_elixir_map, pretty: true) do
+          {:ok,
+           socket
+           |> assign(json: new_json)
+           |> assign(error: nil)
+           |> assign(elixir_map: inspect(new_elixir_map))}
+        else
+          _ -> {:error, "Error when decoding Json to Elixir Map"}
+        end
+
+      true ->
+        {:ok, socket}
     end
   end
 end
--- test/ex_json_parser_web/live/home_live_test.exs
+++ test/ex_json_parser_web/live/home_live_test.exs
@@ -29,4 +29,31 @@ defmodule ExJsonParserWeb.HomeLiveTest do
              |> render_submit(%{"json" => ~s({"ok" "test"})}) =~ "Error"
     end
   end
+
+  describe "testing the elixir map to json submit" do
+    test "should transform to elixir map because json is right", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      assert view
+             |> element("form")
+             |> render_submit(%{"json" => ~s({"ok": "test"})}) =~
+               ~s(%{&amp;quot;ok&amp;quot; =&amp;gt; &amp;quot;test&amp;quot;})
+    end
+
+    test "should not have an Error because json is right", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      refute view
+             |> element("form")
+             |> render_submit(%{"json" => ~s({"ok": "test"})}) =~ "Error"
+    end
+
+    test "should return an error because json is wrong", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      assert view
+             |> element("form")
+             |> render_submit(%{"json" => ~s({"ok" "test"})}) =~ "Error"
+    end
+  end
 end
